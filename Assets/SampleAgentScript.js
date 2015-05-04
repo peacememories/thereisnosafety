f942bb140844cb8efa57cf97040e42ebc6c225ce
Rene Koller <r.koller@me.com>
2015-05-04 21:08:50 +0200

    first randomly created navmesh agents running to a random target

---
+++ Assets/SampleAgentScript.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+
+function Start () 
+{
+	var agent: NavMeshAgent = GetComponent.<NavMeshAgent>();
+    agent.SetDestination(GameObject.Find("target").transform.position);
+}
---
+++ Assets/init.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var floor;
+
+function Start () 
+{
+	floor = GameObject.Find("floor");
+	
+	createTargets();
+
+	for(var i = 0; i < 4; i++)
+	{
+		createPerson(i);
+	}
+}
+
+function createPerson(index)
+{
+	var person = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+	var agent  = person.AddComponent.<NavMeshAgent>() as NavMeshAgent;
+	
+	// TODO set position of person
+	
+	var randomTarget = "target_" + Random.Range(1,4);
+	
+	Debug.Log(randomTarget);
+	
+	agent.SetDestination(GameObject.Find(randomTarget).transform.position);
+}
+
+function createTargets()
+{
+ // TODO create targets here or in visual editor?
+}
+
+function Update () 
+{
+
+}
