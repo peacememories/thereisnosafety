7a239cae3680826f3daf8bd43fbb0e9c60b2e154
Rene Koller <r.koller@me.com>
2015-05-05 21:30:15 +0200

    displaying random events

---
+++ Assets/project/random_event.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var EventText: UnityEngine.UI.Text;
+var Events : String[] = [
+		"Carbomb",
+		"Kidnapping",
+		"more.."
+	];
+
+
+function Start () 
+{
+	EventText = GameObject.Find("Canvas").GetComponentsInChildren(UnityEngine.UI.Text)[0];
+	InvokeRepeating("DisplayEvent", 5, 5);
+}
+
+function DisplayEvent() 
+{
+	EventText.text = Events[Random.Range(0, Events.Length)];
+}
