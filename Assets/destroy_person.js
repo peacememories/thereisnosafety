a50f58030abed8b9278c62495bc9c357e933e5ce
Rene Koller <r.koller@me.com>
2015-05-05 20:20:46 +0200

    Working on collisions

---
+++ Assets/destroy_person.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function OnCollisionEnter (collision : Collision)
+{
+	Debug.Log("collision");
+	Destroy(collision.gameObject);
+}
+
+function OnTriggerEnter (other : Collider) 
+{
+	Debug.Log("trigger");
+	Destroy(other.gameObject);
+}
--- Assets/init.js
+++ Assets/init.js
@@ -1,39 +1,28 @@
 ﻿#pragma strict
 
-var floor;
-
 function Start () 
 {
-	floor = GameObject.Find("floor");
-	
-	createTargets();
-
-	for(var i = 0; i < 4; i++)
+	for(var i = 0; i < 10; i++)
 	{
-		createPerson(i);
+		createPerson();
 	}
 }
 
-function createPerson(index)
+function createPerson()
 {
 	var person = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 	var agent  = person.AddComponent.<NavMeshAgent>() as NavMeshAgent;
+	person.AddComponent.<SphereCollider>();
 	
 	// TODO set position of person
 	
-	var randomTarget = "target_" + Random.Range(1,4);
-	
-	Debug.Log(randomTarget);
+	var randomTarget = "target_" + Random.Range(1,5);
 	
 	agent.SetDestination(GameObject.Find(randomTarget).transform.position);
 }
 
-function createTargets()
-{
- // TODO create targets here or in visual editor?
-}
-
 function Update () 
 {
-
+	// TODO create more persons
 }
+
