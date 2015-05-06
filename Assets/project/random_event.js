a1430198c64941c5f4ed72475a8fb6da65225b8d
Udo Kotzian <udokotzian@gmail.com>
2015-05-06 19:49:16 +0200

    10 news messages added

--- Assets/project/random_event.js
+++ Assets/project/random_event.js
@@ -2,16 +2,22 @@
 
 var EventText: UnityEngine.UI.Text;
 var Events : String[] = [
-		"Carbomb",
-		"Kidnapping",
-		"more.."
+		"A carbomb exploded! 5 dead, 20 injured.",
+		"An Austrian tourist group kidnapped!",
+		"Airplane hijacked!",
+		"Attack on military base",
+		"Explosion in school",
+		"Suicide bombing at a store",
+		"Attack on newspaper office",
+		"28 people killed in terror attack",
+		"Prime Minister assassinated!",
+		"Attack on synagogue"
 	];
-
 
 function Start () 
 {
 	EventText = GameObject.Find("Canvas").GetComponentsInChildren(UnityEngine.UI.Text)[0];
-	InvokeRepeating("DisplayEvent", 1, 5);
+	InvokeRepeating("DisplayEvent", 1, 15);
 }
 
 function DisplayEvent() 
